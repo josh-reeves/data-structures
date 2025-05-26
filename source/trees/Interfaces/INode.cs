c754fb046ad4ecbe5bd70ec726fd37a9e5ea7aef
Joshua Reeves <jr.accounts@joshreeves.net>
2025-05-26 03:57:52 -0500

    Began working on Tree data structures in earnest.

---
+++ source/trees/Interfaces/INode.cs
@@ -0,0 +1,13 @@
+namespace Trees;
+
+public interface INode<NodeType>
+{
+    public INode<NodeType>[]? Children { get; set; }
+
+    /// <summary>
+    /// Peforms a recursive deep copy of the node.
+    /// </summary>
+    /// <returns>A new node with identical copies of the source nodes properties.</returns>
+    public INode<NodeType> Copy();
+
+}
--- source/trees/Tree.cs
+++ source/trees/Tree.cs
@@ -1,6 +1,9 @@
-﻿namespace Trees;
+﻿using System.Diagnostics;
+using System.Text;
 
-public abstract class Tree<TreeType>
+namespace Trees;
+
+public abstract class Tree<TreeType> : ITree<TreeType>
 {
     #region Constructor(s)
     public Tree() { }
@@ -8,15 +11,37 @@ public abstract class Tree<TreeType>
     #endregion
 
     #region Classes
-    public class Node
+    public class Node<NodeType> : INode<NodeType>
     {
         public Node() { }
 
         #region Properties
-        public Node[]? Children { get; set; }
+        public INode<NodeType>[]? Children { get; set; }
 
         #endregion
-        
+
+        #region Methods
+        public INode<NodeType> Copy()
+        {
+            INode<NodeType> source = this,
+                            copy = new Node<NodeType>();
+
+            if (source.Children is not null)
+            {
+                copy.Children = new INode<NodeType>[source.Children.Length];
+
+                for (int i = 0; i < source.Children.Length; i++)
+                    copy.Children[i] = source.Children[i].Copy();
+
+
+            }
+
+            return copy;
+
+        }
+
+        #endregion
+    
     }
 
     #endregion
